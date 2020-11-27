0c974434ac92059f7e03bd69e9ff910406ee7c65
toniozi <hygiaphone66@gmail.com>
2020-11-27 10:28:14 +0000

    1st commit

---
+++ popup/edit.js
@@ -0,0 +1,7 @@
+function editOn(){
+  document.body.contentEditable = 'true'; document.designMode='on'; void 0
+}
+
+function editOff(){
+  document.body.contentEditable = 'false'; document.designMode='off'; void 0
+}
